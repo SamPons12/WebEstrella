394f68a5bfa185639032a58cc6c4426c2b6c3d5b
sam-pc <sampf2006@gmail.com>
2025-03-21 10:19:17 +0100

    Finalizado

--- js/index.js
+++ js/index.js
@@ -2,3 +2,19 @@ document.getElementById("corazon").addEventListener("click", function() {
   document.getElementById("mensaje").style.display = "flex";
   document.getElementById("back").style.display = "block";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btnNo = document.getElementById("no");
+
+  btnNo.addEventListener("mouseover", () => {
+      const maxX = window.innerWidth - btnNo.clientWidth;
+      const maxY = window.innerHeight - btnNo.clientHeight;
+
+      const randomX = Math.floor(Math.random() * maxX);
+      const randomY = Math.floor(Math.random() * maxY);
+
+      btnNo.style.position = "absolute";
+      btnNo.style.left = `${randomX}px`;
+      btnNo.style.top = `${randomY}px`;
+  });
+});
